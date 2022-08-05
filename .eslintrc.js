1e01a07a0bbb75f20530c6938006407e043bd279
satoshionoda <satoshionoda@gmail.com>
2022-08-05 17:08:42 +0900

    lint 修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "import/order": [
       "error",
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   ignoreFiles: [".git/**", "dist/**", "**/node_modules/**"],
   plugins: ["stylelint-scss", "stylelint-order"],
   rules: {
+    "comment-no-empty": null,
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
     "rule-empty-line-before": ["always", { except: ["first-nested"] }],
@@ -16,11 +17,13 @@ module.exports = {
       "always",
       {
         except: ["after-same-name", "first-nested"],
-        ignore: ["after-comment"],
+        ignore: ["after-comment", "before-comment"],
         ignoreAtRules: ["mixin", "function"],
       },
     ],
     "scss/at-use-no-unnamespaced": true,
     "scss/double-slash-comment-whitespace-inside": null,
+    "scss/double-slash-comment-empty-line-before": null,
+    "scss/comment-no-empty": null,
   },
 };
