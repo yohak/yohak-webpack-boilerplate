71e0bb874ccaf467a542fc3bb21daab2eb839183
Satoshi Onoda <satoshionoda@gmail.com>
2024-03-05 16:01:51 +0900

    huskey

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: ["@prettier/plugin-pug"],
+  plugins: ["@prettier/plugin-pug", "@prettier/plugin-php"],
 
   semi: true,
   trailingComma: "es5",
@@ -7,4 +7,6 @@ module.exports = {
   tabWidth: 2,
   pugPrintWidth: 9999,
   printWidth: 100,
+  phpVersion: "7.4",
+  braceStyle: "1tbs",
 };
